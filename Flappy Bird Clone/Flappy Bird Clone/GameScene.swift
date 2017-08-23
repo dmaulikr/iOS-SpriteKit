e9290b26eefc7c540bdb8c1f20e0a9d0ceb1c293
alwongg <wong_1994@hotmail.com>
2017-08-22 20:28:11 -0500

    Add control to bird with impulse applied

--- Flappy Bird Clone/Flappy Bird Clone/GameScene.swift
+++ Flappy Bird Clone/Flappy Bird Clone/GameScene.swift
@@ -43,30 +43,56 @@ class GameScene: SKScene {
             
             
         }
-            
-            // now add both bird textures and create the animation of bird flapping then repeat forever
-            let birdTexture = SKTexture(imageNamed: "flappy1.png")
-            let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
-            
-            let animation = SKAction.animate(with: [birdTexture, birdTexture2], timePerFrame: 0.1)
-            let makeBirdFlap = SKAction.repeatForever(animation)
-            
-            bird = SKSpriteNode(texture: birdTexture)
-            bird.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
-            
-            bird.run(makeBirdFlap)
-            
-            self.addChild(bird)
-            
-        }
         
-        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-            
-        }
+        // now add both bird textures and create the animation of bird flapping then repeat forever
+        let birdTexture = SKTexture(imageNamed: "flappy1.png")
+        let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
+        
+        let animation = SKAction.animate(with: [birdTexture, birdTexture2], timePerFrame: 0.1)
+        let makeBirdFlap = SKAction.repeatForever(animation)
+        
+        bird = SKSpriteNode(texture: birdTexture)
+      
+        bird.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        
+        bird.run(makeBirdFlap)
+        
+        self.addChild(bird)
+        
+        // create the ground
+        
+        // invisible object, no real images used to create the ground
+        let ground = SKNode()
+        
+        ground.position = CGPoint(x: self.frame.midX, y: -self.frame.height / 2)
+        
+        ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: self.frame.width, height: 1))
+        
+        // not affected by gravity, so won't fall
+        ground.physicsBody?.isDynamic = false
+        
+        self.addChild(ground)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let birdTexture = SKTexture(imageNamed: "flappy1.png")
+        
+        // add physics interaction to bird i.e. gravity and touch to fly
+        
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: birdTexture.size().height / 2)
+      
+        bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 80))
         
         
         
-        override func update(_ currentTime: TimeInterval) {
-            // Called before each frame is rendered
-        }
+    }
+    
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
 }
